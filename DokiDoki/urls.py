e2166af96575c19e8acc54b8bf58865d765877ef
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2021-10-23 23:30:37 +0330

    initial project

--- DokiDoki/urls.py
+++ DokiDoki/urls.py
@@ -13,15 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from User.views import RegisterAPI
-from django.urls import path
-
-
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/register/', RegisterAPI.as_view(), name='register'),
 ]
