7d2012eb4ed46b1bd43ab47dd8988a8d1d45523e
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2021-11-02 00:57:35 +0330

    email templates' style

--- DokiApp/urls.py
+++ DokiApp/urls.py
@@ -1,4 +1,3 @@
-from django.conf.urls import url
 from django.urls import path
 
 from rest_framework.authtoken.views import obtain_auth_token
@@ -9,7 +8,7 @@ EMAIL_TOKEN = 'c754wcnr0f7c4cwefm-qxgtvv5409wxw,^#4-2!0*erpmcwef#$t43mumc##r$,9P
 
 urlpatterns = [
     path('sign_up', SignUp.as_view(), name='SignUp'),
-    url('^check_username/(?P<username>.+)/$', view=CheckUsername.as_view(), name='CheckUsername'),
+    path('^check_username/(?P<username>.+)/$', view=CheckUsername.as_view(), name='CheckUsername'),
     path('login', LogIn.as_view(), name="LogIn"),
     path('logout', LogOut.as_view(), name='LogOut'),
     path('verify_email', VerifyEmail.as_view(), name='VerifyEmail'),
