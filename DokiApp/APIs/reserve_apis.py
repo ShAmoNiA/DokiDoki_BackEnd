34806cdef8376908af18253886508d2757b88b32
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2021-12-24 21:54:31 +0330

    refactor ReserveDoctor

--- DokiApp/APIs/reserve_apis.py
+++ DokiApp/APIs/reserve_apis.py
@@ -1,30 +1,34 @@
+import datetime
+
+from django.shortcuts import get_object_or_404
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+
 from ..Helper_functions.email_functions import send_reserve_message
+from ..permissions import IsPatient
 from ..serializers import *
-import datetime
 
 
 class ReserveDoctor(APIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsPatient)
 
-    def post(self, request,):
-        if not request.user.is_authenticated:
-            return Response({'success': False, 'message': 'Please login first'})
+    def post(self, request, ):
         time = request.POST['time']
         date_str = request.POST['date']
         doctor_id = request.POST['doctor_id']
-        doctors = DoctorProfile.objects.filter(id=doctor_id)
+
+        doctor = get_object_or_404(DoctorProfile, id=doctor_id)
         date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d')
-        if len(doctors) == 0:
-            return Response({'success': False, 'message': 'Doctor not found'})
-        doctor = doctors.first()
-        reserve = Reserve(doctor=doctor, user=request.user, time=time, date=date_obj)
-        reserve.save()
+
+        Reserve.objects.create(doctor=doctor, user=request.user, time=time, date=date_obj)
         send_reserve_message(doctor.user.email, doctor.user.username, date_str + " " + time, request.user.fullname)
+
         return Response({'success': True, 'message': 'Reserved!'})
 
+
 class ReservesList(APIView):
+
     def get(self):
-        return Response({'success' : True, 'message' : 'Not available for now'})
+        return Response({'success': True, 'message': 'Not available for now'})
