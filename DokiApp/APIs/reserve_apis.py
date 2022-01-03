6d8e68282fccff66d8b2fead335f8ea3628a1326
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2022-01-03 21:23:39 +0330

    save chat while reserve is created

--- DokiApp/APIs/reserve_apis.py
+++ DokiApp/APIs/reserve_apis.py
@@ -19,19 +19,34 @@ from ..permissions import IsPatient, IsDoctor
 from ..serializers import *
 
 
+# TODO: move the function to another place
+def create_chat_object_in_db(patient, doctor):
+    user_1_id = patient.user.id
+    user_2_id = doctor.user.id
+
+    name = str(user_1_id) + "_" + str(user_2_id)
+    if user_1_id > user_2_id:
+        name = str(user_2_id) + "_" + str(user_1_id)
+
+    Chat.objects.create(name=name, patient=patient, doctor=doctor)
+
+
 class ReserveDoctor(APIView):
     permission_classes = (IsAuthenticated, IsPatient)
 
-    def post(self, request, ):
+    def post(self, request):
         time = request.POST['time']
         date_str = request.POST['date']
         doctor_id = request.POST['doctor_id']
 
-        doctor = get_object_or_404(DoctorProfile, id=doctor_id)
+        doctorProfile = get_object_or_404(DoctorProfile, id=doctor_id)
         date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d')
 
-        Reserve.objects.create(doctor=doctor, user=request.user, time=time, date=date_obj)
-        send_reserve_message(doctor.user.email, doctor.user.username, date_str + " " + time, request.user.fullname)
+        Reserve.objects.create(doctor=doctorProfile, user=request.user, time=time, date=date_obj)
+        send_reserve_message(doctorProfile.user.email, doctorProfile.user.username,
+                             date_str + " " + time, request.user.fullname)
+
+        create_chat_object_in_db(request.user.profile, doctorProfile)
 
         return Response({'success': True, 'message': 'Reserved!'})
 
--- DokiApp/consumers.py
+++ DokiApp/consumers.py
@@ -1,11 +1,13 @@
 import json
 
 from asgiref.sync import async_to_sync, sync_to_async
+from channels.db import database_sync_to_async
+
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.auth import login, logout
 
 from rest_framework.authtoken.models import Token
-from .models import User
+from .models import User, DoctorProfile, PatientProfile, Chat, Message
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -16,20 +18,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
             token = self.scope['url_route']['kwargs']['auth_token']
             self.user = await self.get_user_by_token(token)
             await login(self.scope, self.user)
+
+            partner_username = self.scope['url_route']['kwargs']['partner_username']
+            self.partner_user = await self.get_user_by_username(partner_username)
         except:
             await self.close(code=403)
+            return
 
-        partner_username = self.scope['url_route']['kwargs']['partner_username']
-        self.group_name = await self.generate_group_name(self.user.username, partner_username)
+        self.group_name = await self.generate_group_name(self.user.username, self.partner_user.username)
+        if not await self.chat_exists_by_name(self.group_name):
+            await self.close(code=403)
+            return
+        self.chat = await self.get_chat_by_name(self.group_name)
 
         await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, code):
-        await self.channel_layer.group_discard(
-            self.group_name,
-            self.channel_name
-        )
+        try:
+            await self.channel_layer.group_discard(
+                self.group_name,
+                self.channel_name
+            )
+        except:
+            pass
 
     async def receive(self, text_data=None, bytes_data=None):
         if text_data:
@@ -52,6 +64,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
     @sync_to_async
     def get_user_by_token(self, token):
         return Token.objects.get(key=token).user
+
+    @sync_to_async
+    def get_user_by_username(self, username):
+        return User.objects.get(username=username)
+
+    @sync_to_async
+    def get_chat_by_name(self, name):
+        return Chat.objects.get(name=name)
+
+    @database_sync_to_async
+    def chat_exists_by_name(self, name):
+        return bool(Chat.objects.filter(name=name).count())
 
     @sync_to_async
     def generate_group_name(self, username_1, username_2):
