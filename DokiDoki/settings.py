1da46e0a1b7f9d4ee5c1e7fc3f1124d7930cfa4f
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2022-01-11 02:13:04 +0330

    config redis for channels

--- DokiDoki/settings.py
+++ DokiDoki/settings.py
@@ -100,8 +100,11 @@ TEMPLATES = [
 ASGI_APPLICATION = 'DokiDoki.routing.application'
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
 
 
